4395d69814cc5ed6386930412d7e9c30d6568c4a
Bolous AbuJaber <ajbolous@gmail.com>
2018-11-10 23:51:41 +0200

    cleaned up server run file

--- database/src/server.cc
+++ database/src/server.cc
@@ -3,29 +3,12 @@
 #include "common/manager/server_manager.h"
 #include "stubs/query_api.impl.h"
 
-void InitServerData()
-{
-	std::fstream output("db.buf", std::ios::out | std::ios::binary);
-	common::protos::Entity e;
-	e.set_name("Bolous2");
-	e.set_id("1");
-	e.set_password("12356");
-	if (!e.SerializeToOstream(&output))
-		logging::Error("Failed to read Entity buffer.");
-	output.close();
-}
-
 int main(int argc, char **argv)
 {
 	std::string server_address("localhost:60060");
 	std::string db_workspace("db");
-	
-	InitServerData();
+
 	core::DatabaseCore dbCore(db_workspace);
-	common::protos::Entity entity;
-	entity.set_id("123232-123");
-	entity.set_name("bolous");
-	dbCore.WriteEntity(entity);
 	manager::ServerManager server(server_address);
 	server.AddService(new QueryApiImpl(&dbCore), "QueryApiImpl");
 	server.Start();
--- database/src/test.cc
+++ database/src/test.cc
@@ -4,32 +4,32 @@
 #include <grpcpp/channel.h>
 
 #include "database/protos/query_api.grpc.pb.h"
+#include "common/logging/logging.h"
+
+common::protos::Entity CreateTestEntity()
+{
+    common::protos::Entity entity;
+
+    entity.set_id("entity id");
+    entity.set_name("bolous-test");
+    entity.set_address("address test");
+    entity.set_password("password3232");
+    return entity;
+}
 
 int main(int argc, char **argv)
 {
     auto channel = grpc::CreateChannel("localhost:60060", grpc::InsecureChannelCredentials());
     std::unique_ptr<database::protos::QueryApi::Stub> stub = database::protos::QueryApi::NewStub(channel);
-    grpc::ClientContext(context);
 
     database::protos::QueryRequest request;
     request.set_id("entity id");
 
-    common::protos::Entity e1;
-
-    e1.set_id("entity id");
-    e1.set_name("bolous-test");
-    e1.set_address("address test");
-    e1.set_password("password");
-
     database::protos::UpdateResponse response;
-    stub->Insert(&context, e1, &response);
 
-    grpc::ClientContext cont2;
+    common::protos::Entity entity = CreateTestEntity();
 
-    common::protos::Entity entity;
-    std::cout << (stub->Query(&cont2, request, &entity).ok() ? "Success" : "Failed") << std::endl;
-
-    std::cout << entity.name() << std::endl;
-    std::cout << entity.password() << std::endl;
-    std::cout << entity.address() << std::endl;
+    if (stub->Insert(new grpc::ClientContext(), entity, &response).ok())
+        if (stub->Query(new grpc::ClientContext(), request, &entity).ok())
+            logging::Info("Name: " + entity.name() + ", Password: " + entity.password());
 }
